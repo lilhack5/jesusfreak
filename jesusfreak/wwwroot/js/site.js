6b4a7774d7afec8437c077abf7edb0fd5a10d420
lilhack5 <114317539+lilhack5@users.noreply.github.com>
2023-04-17 21:14:48 -0700

    Initial commit

---
+++ jesusfreak/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function newColor() {
+    document.getElementById("jesus_freak").style.color = "red";
+}
+
+function oldColor() {
+    document.getElementById("jesus_freak").style.color = "black";
+}
